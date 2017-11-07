ba625f7664427583dfb9985cb05b1ddebcb02491
VTunes <machadop.vinicius@hotmail.com>
2017-11-07 20:19:47 +1300

    Add files via upload

---
+++ main/java/com/br/vtunes7/MusicController.java
@@ -0,0 +1,16 @@
+package com.br.vtunes7;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by Vinicius on 09/02/2017.
+ */
+public class MusicController extends MediaController {
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+}
---
+++ main/java/com/br/vtunes7/Song.java
@@ -0,0 +1,22 @@
+package com.br.vtunes7;
+
+/**
+ * Created by Vinicius on 09/02/2017.
+ */
+
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+}
